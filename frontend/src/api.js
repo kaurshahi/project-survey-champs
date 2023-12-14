a976c40282768e569822f941efa0000f9fa97204
Manpreet Dhaliwal <kaurshahi1@gmail.com>
2023-12-13 19:24:59 -0500

    update production base Url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+const apiBaseUrl =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: apiBaseUrl,
 });
 
 api.interceptors.response.use(
